ec8c434d4e5199a223072c17dcfea80790807871
Anthony <acgardner95@gmail.com>
2023-11-14 16:31:00 -0600

    remove venv from test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,5 @@ set -eou pipefail
 
 readonly CWD=$(realpath .)
 readonly TESTS_DIR=$(realpath $CWD/tests)
-readonly VENV_DIR=$(realpath $CWD/.venv)
 
-source $VENV_DIR/bin/activate
 python3 -m pytest $TESTS_DIR
