6eb520dbbeb3634cda2e66d05cf596e77d10d3b3
Anthony <acgardner95@gmail.com>
2023-11-15 17:54:35 -0600

    remove old code

--- src/sensors/imu.h
+++ src/sensors/imu.h
@@ -9,18 +9,11 @@ namespace sensors {
 
     class IMU {
     public:
-      // Vector3f specific_force;
-      //
-      // IMU(Vector3f angular_velocity, specific_force) {
-      //   this->angular_velocity = angular_velocity;
-      //   this->specific_force = specific_force;
-      // }
       float wx;
       float wy;
 
       IMU(float wx_, float wy_);
 
-      // float get_wx();
     private:
       float _wx;
       float _wy;
